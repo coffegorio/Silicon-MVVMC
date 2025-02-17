97290dff84d4f69124ed6156c2c5d7f192d74390
coffegorio <coffegorio@vk.com>
2025-02-18 00:48:06 +0300

    Initial Commit

---
+++ SiliconApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiliconApplication
+//
+//  Created by Егорио on 18.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
